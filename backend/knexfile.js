4f64cd86dd5bea2a61055b853a35b867477f1165
Racklyn <racklynsotero@gmail.com>
2020-04-20 22:28:38 -0300

    .env file on gitignore

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,14 +1,10 @@
-// Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
   development: {
-    client: 'postgresql',
-    connection: {
-      filename: './src/database/listingBD.pg',
-      user:'postgres',
-      password:'postgres',
-      database:'listingBD'
-    },
+    client: 'pg',
+    //"postgres://YourUserName:YourPassword@localhost:5432/YourDatabase"
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 100,
